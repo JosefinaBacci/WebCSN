321fefdd95b132cff3926b02621878d1f6291988
JosefinaBacci <josefinabacci015@gmail.com>
2026-02-20 22:46:58 -0300

    removed usenavigate import in authcontext

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,5 +1,4 @@
 import { createContext, useContext, useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { authEvents } from "../api/eventEmitter";
 
 type AuthContextType = {
