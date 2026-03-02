e8d3e44d15f84061c2f1efad7acb85ac6456f47e
JosefinaBacci <josefinabacci015@gmail.com>
2026-03-02 20:15:45 -0300

    deleted an image imported but not used it

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,7 +7,6 @@ import SectionCard from "../shared/components/SectionCard";
 import img0 from "../images/img0.jpg";
 import img1 from "../images/img1.jpg";
 import img2 from "../images/img2.jpg";
-import img3 from "../images/img3.jpg";
 import img4 from "../images/img4.jpg";
 import img5 from "../images/img5.jpg";
 import img7 from "../images/img7.jpg";
